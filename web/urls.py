3a2e20d77d8d12649a62848c3eab8fd67589a826
Vivek <dvivekanandhan@hotmail.com>
2023-02-21 11:03:57 +0530

    styles

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    # path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', views.index, name='index'),
 ]
