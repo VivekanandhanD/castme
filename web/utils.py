84e806f77997d9ad4342b071d28fc95209f966c3
Vivek <dvivekanandhan@hotmail.com>
2023-02-23 18:54:39 +0530

    s3 added

---
+++ web/utils.py
@@ -0,0 +1,8 @@
+import boto3
+from django.conf import settings
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=settings.AWS_ACCESS_KEY,
+    aws_secret_access_key=settings.AWS_SECRET_KEY
+)
--- web/views.py
+++ web/views.py
@@ -3,12 +3,14 @@ from django.contrib.auth.decorators import login_required
 from allauth.account.views import LoginView
 from .forms import CustomLoginForm
 from .es import es_client
+from .utils import *
 
 @login_required
 def index(request):
     print(es_client.info())
     resp = es_client.get(index="customer", id=1)
     print(resp['_source'])
+    s3_client.download_file('cast.me', 'signature.png', 'signature.png')
     return render(request, 'index.html', {'data':resp})
 
 
